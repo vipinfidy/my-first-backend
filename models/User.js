b190d5700b6c37e651fbd9d8d5d886d1f20dcb21
vipinfidy <vipinfidypay@gmail.com>
2025-06-20 16:06:59 +0530

    add-new-schemadata

--- models/User.js
+++ models/User.js
@@ -4,7 +4,16 @@ const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
-  }
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
